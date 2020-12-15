6789d10fcc1a1363d332ec363805c0d1f1ad13d3
Lua Carlos Martins <luacmartins@Luas-MacBook-Pro.local>
2020-12-15 15:39:36 -0700

    add new accounts

--- components/shared/Card/Card.js
+++
@@ -1,9 +0,0 @@
-const Card = ({ children, className, ...props }) => {
-   return (
-      <div {...props} className={`bg-white rounded-lg shadow-theme ${className}`}>
-         {children}
-      </div>
-   );
-}
-
-export default Card;
